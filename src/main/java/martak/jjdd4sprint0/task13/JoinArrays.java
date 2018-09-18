cf74432c08adb179ee60f4fd640b055b90ecfc9a
Marta K <martakostyk@op.pl>
2018-09-18 13:17:41 +0200

    task13 - first solution with ArrayUtils

--- src/main/java/martak/jjdd4sprint0/task13/JoinArrays.java
+++ src/main/java/martak/jjdd4sprint0/task13/JoinArrays.java
@@ -1,11 +1,61 @@
 package martak.jjdd4sprint0.task13;
 
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.*;
+
 public class JoinArrays {
+
+    private Scanner scanner = new Scanner(System.in);
 
     public int[] join(int[] array1, int[] array2) {
 
-        return null;
+        int[] joined = ArrayUtils.addAll(array1, array2);
+
+        return Arrays.stream(joined)
+                .distinct()
+                .boxed()
+                .sorted((n1, n2) -> Integer.compare(n2, n1))
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 
+    public int[] getArray(String message) {
 
+        int[] array;
+
+        System.out.println(message);
+
+        while (true) {
+            try {
+                System.out.println("Array size: ");
+                array = new int[Integer.valueOf(scanner.next())];
+                break;
+            } catch (Exception e) {
+                System.out.println("Given value is incorrect");
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            while (true) {
+                try {
+                    System.out.println("Number" + i + ": ");
+                    array[i] = Integer.valueOf(scanner.next());
+                    break;
+                } catch (Exception e) {
+                    System.out.println("Given value is incorrect");
+                }
+            }
+        }
+
+        return array;
+    }
+
+    public static void main(String[] args) {
+        JoinArrays joinArrays = new JoinArrays();
+        int[] array1 = joinArrays.getArray("First array");
+        int[] array2 = joinArrays.getArray("Second array");
+
+        System.out.println(Arrays.toString(joinArrays.join(array1, array2)));
+    }
 }
--- src/test/java/martak/jjdd4sprint0/task13/JoinArraysTest.java
+++ src/test/java/martak/jjdd4sprint0/task13/JoinArraysTest.java
@@ -1,32 +1,27 @@
 package martak.jjdd4sprint0.task13;
 
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JoinArraysTest {
 
-    private JoinArrays joinArrays;
-
-    @BeforeAll
-    public void setUp() {
-        this.joinArrays = new JoinArrays();
-    }
+    private JoinArrays joinArrays = new JoinArrays();
 
     @Test
-    public void shouldJoinArraysWithoutDuplicatesAndSort() {
+    public void join1MethodTest() {
         // given
         int[] array1 = new int[]{1,4,7,0};
         int[] array2 = new int[]{2,0};
 
         // when
         int[] result = joinArrays.join(array1, array2);
+        int[] expected = new int[]{7,4,2,1,0};
 
         // then
-        int[] expected = new int[]{7,4,2,1,0};
-        assertEquals(expected, result);
-
+        assertTrue(Arrays.equals(expected, result));
     }
 
 }
