b8ae4d3eeb90e610ff41a2cc88d297f98585f862
Marta K <martakostyk@op.pl>
2018-09-19 18:17:33 +0200

    task 2 - finish calculator

--- src/main/java/martak/jjdd4sprint0/task2/Calculator.java
+++ src/main/java/martak/jjdd4sprint0/task2/Calculator.java
@@ -1,6 +1,9 @@
 package martak.jjdd4sprint0.task2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Calculator {
 
@@ -15,12 +18,11 @@ public class Calculator {
         }
     }
 
-    public MathOperation getChoice() {
+    public MathOperation getOperation() {
 
         while (true) {
 
             try {
-
                 System.out.println("Select operation: ");
 
                 switch (Integer.valueOf(scanner.next())) {
@@ -42,10 +44,122 @@ public class Calculator {
         }
     }
 
+    public int getNumberOfParameters() {
+
+        while (true) {
+            try {
+                System.out.println("Number of parameters: ");
+                int input = Integer.valueOf(scanner.next());
+                if (input <= 1) {
+                    System.out.println("Value has to be bigger than 1");
+                } else {
+                    return input;
+                }
+            } catch (Exception e) {
+                System.out.println("Given value is incorrect. Please enter correct value");
+            }
+        }
+    }
+
+    public double getParameter() {
+
+        while (true) {
+            try {
+                System.out.println("Enter parameter: ");
+                return Double.valueOf(scanner.next());
+            } catch (Exception e) {
+                System.out.println("Given value is incorrect. Please enter correct value");
+            }
+        }
+    }
+
+    public void calculate(MathOperation mathOperation, List<Double> parameters) {
+
+        String arithmeticOperator = null;
+        double result = 0;
+
+        switch (mathOperation) {
+            case SUM:
+                arithmeticOperator = "+";
+                result = sum(parameters);
+                break;
+            case SUBTRACT:
+                arithmeticOperator = "-";
+                result = subtract(parameters);
+                break;
+            case MULTIPLY:
+                arithmeticOperator = "*";
+                result = multiply(parameters);
+                break;
+            case DIVIDE:
+                arithmeticOperator = "/";
+                result = divide(parameters);
+                break;
+        }
+
+        System.out.println("Operation: " + parameters
+                .stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(arithmeticOperator)));
+
+        System.out.println("Result: " + result);
+    }
+
+    private double sum(List<Double> parameters) {
+
+        double result = parameters.get(0);
+        for (int i = 1; i < parameters.size(); i++) {
+            result += parameters.get(i);
+        }
+        return result;
+    }
+
+    private double subtract(List<Double> parameters) {
+
+        double result = parameters.get(0);
+        for (int i = 1; i < parameters.size(); i++) {
+            result -= parameters.get(i);
+        }
+        return result;
+    }
+
+    private double multiply(List<Double> parameters) {
+
+        double result = parameters.get(0);
+        for (int i = 1; i < parameters.size(); i++) {
+            result *= parameters.get(i);
+        }
+        return result;
+    }
+
+    private double divide(List<Double> parameters) {
+
+        double result = parameters.get(0);
+        for (int i = 1; i < parameters.size(); i++) {
+            if (parameters.get(i) == 0) {
+                throw new IllegalArgumentException("Cannot divide by zero");
+            }
+
+            result /= parameters.get(i);
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
+
         calculator.displayMenu();
-        MathOperation choice = calculator.getChoice();
+
+        MathOperation operation = calculator.getOperation();
+
+        int numOfParameters = calculator.getNumberOfParameters();
+        List<Double> parameters = new ArrayList<>();
+
+        for (int i = 0; i < numOfParameters; i++) {
+            parameters.add(calculator.getParameter());
+        }
+
+        calculator.calculate(operation, parameters);
     }
 }
